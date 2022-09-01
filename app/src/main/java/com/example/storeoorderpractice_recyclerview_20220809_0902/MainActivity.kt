626b28f7ce3e2cd1a57944fa9e2ed49e7cd6a80e
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-02 04:26:25 +0900

    잠시

--- app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/MainActivity.kt
+++ app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.storeoorderpractice_recyclerview_20220809_0902
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.example.storeoorderpractice_recyclerview_20220809_0902.adapters.MainViewPagerAdapter
 import com.example.storeoorderpractice_recyclerview_20220809_0902.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var mPagerAdapter : MainViewPagerAdapter
 
 //  onCreate 내부에는 필수적으로 작성해야 하는 것 외에는 비워준다
 //  coz 생명주기 때문 함수로 묶어서 작업해야하는것에 익숙해져야 하므로
@@ -24,7 +26,9 @@ class MainActivity : BaseActivity() {
         }
 //      모든 것의 초기화는 여기서 할 거다
         override fun setValues() {
-
+            mPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+            binding.mainViewPager.adapter =  mPagerAdapter
+            binding.mainTabLayout.setupWith
         }
 
 
