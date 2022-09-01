d1b3fad65bae636fcb6080fe942cb1a7857bdd73
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-02 03:53:00 +0900

    탭 레이아웃과 뷰페이저 태그 배치후 아이디 부여

--- app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/BaseActivity.kt
+++ app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/BaseActivity.kt
@@ -3,10 +3,13 @@ package com.example.storeoorderpractice_recyclerview_20220809_0902
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 
+//코틀린은 오직 open/추상 클래스만 상속가능
+//AppCom~클래스를 상속받아야 생명주기함수들과 액티비티들로서 활동 가능
 abstract class BaseActivity : AppCompatActivity() {
 
     val mContext : Context = this
 
+//  추상함수는 오직 추상/오픈 클래스 에서만 생성
     abstract fun setupEvents()
 
     abstract fun setValues()
