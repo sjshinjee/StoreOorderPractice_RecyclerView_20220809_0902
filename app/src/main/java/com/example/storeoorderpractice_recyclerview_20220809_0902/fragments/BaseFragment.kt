74fd8c74f674d6714606eb3ca6eb29624748ccd4
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-02 05:03:36 +0900

    BaseFragment에 생명주기 고려하여 mContext작성

--- app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/fragments/BaseFragment.kt
@@ -1,11 +1,18 @@
 package com.example.storeoorderpractice_recyclerview_20220809_0902.fragments
 
 import android.content.Context
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 abstract class BaseFragment : Fragment() {
 
-    val mContext : Context = requireContext()
+    lateinit var mContext : Context
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mContext = requireContext()
+    }
 
     abstract fun setupEvents()
     abstract fun setValues()
