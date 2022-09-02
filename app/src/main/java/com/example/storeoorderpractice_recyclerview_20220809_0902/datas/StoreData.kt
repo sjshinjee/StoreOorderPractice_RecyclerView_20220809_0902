008ebc8a0a5ee9a9bf0abc94ae4f9dfbf65176c6
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-02 21:04:33 +0900

    리싸이클러뷰 데이터 표현용 클래스 제작

---
+++ app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/datas/StoreData.kt
@@ -0,0 +1,11 @@
+package com.example.storeoorderpractice_recyclerview_20220809_0902.datas
+
+class StoreData(
+    val title : String,
+    val score : Double,
+    val imageUri : String,
+    val phoneNum : String
+
+) {
+
+}
