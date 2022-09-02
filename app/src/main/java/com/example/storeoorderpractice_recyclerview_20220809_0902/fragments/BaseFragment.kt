c733c8541c78f7cc03d393774b9878d4d546872e
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-02 22:51:58 +0900

    필요 데이터 목록 추가 즉 더미 데이터 추가

--- app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/fragments/BaseFragment.kt
@@ -9,6 +9,7 @@ abstract class BaseFragment : Fragment() {
 
     lateinit var mContext : Context
 //생명주기 때문에 온뷰크리에이티드안에 mContext초기화 해줘야 한다
+//coz View가 생겨야 Context가 발생
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mContext = requireContext()
--- app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/fragments/PizzaStoreFragment.kt
+++ app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/fragments/PizzaStoreFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.storeoorderpractice_recyclerview_20220809_0902.R
 import com.example.storeoorderpractice_recyclerview_20220809_0902.databinding.FragmentPizzaStoreBinding
+import com.example.storeoorderpractice_recyclerview_20220809_0902.datas.StoreData
 
 class PizzaStoreFragment : BaseFragment() {
 
     lateinit var binding : FragmentPizzaStoreBinding
+
+    val mList = ArrayList<StoreData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +30,11 @@ class PizzaStoreFragment : BaseFragment() {
     }
 
     override fun setValues() {
+        mList.clear()
 
+        mList.add(StoreData("피자헛", 4.2, "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg" , "1577-8080"))
+        mList.add(StoreData("파파존스", 4.5,  "http://mblogthumb-phinf.pstatic.net/20160530_116/ppanppane_14646177654870uGy9_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%281%29.png?type=w800", "1577-8080"))
+        mList.add(StoreData("도미노피자", 3.7, "https://www.pngplay.com/wp-content/uploads/6/Dominos-Pizza-Logo-Background-PNG-Image.png", "1577-3082"))
+        mList.add(StoreData("미스터피자", 3.5,  "https://mblogthumb-phinf.pstatic.net/20160530_62/ppanppane_14646177047843NP3E_PNG/%B9%CC%BD%BA%C5%CD%C7%C7%C0%DA_%B7%CE%B0%ED_%283%29.png?type=w800", "1577-0077"))
     }
 }
