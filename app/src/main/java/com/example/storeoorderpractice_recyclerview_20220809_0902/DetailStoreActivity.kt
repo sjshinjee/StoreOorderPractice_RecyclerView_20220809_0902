f58663ca0aeec0948efbdbf731caf70dae4be170
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-03 01:34:02 +0900

    세부 상점 데이터 표시용 액티비티

---
+++ app/src/main/java/com/example/storeoorderpractice_recyclerview_20220809_0902/DetailStoreActivity.kt
@@ -0,0 +1,11 @@
+package com.example.storeoorderpractice_recyclerview_20220809_0902
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailStoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_store)
+    }
+}
